6fea3f948a22320d14f7b7989a2b75c6914ff663
Balasaheb Kadam <bala@Balasahebs-MacBook-Air.local>
2026-08-22 15:43:25 +0900

    chore(release): bump version to v1.6.1

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,5 +1,5 @@
 // App & Deployment Version Tracking
-export const APP_VERSION = 'v1.6.0';
-export const BUILD_TIMESTAMP = '2026-08-22 15:00';
+export const APP_VERSION = 'v1.6.1';
+export const BUILD_TIMESTAMP = '2026-08-22 15:43';
 
 
